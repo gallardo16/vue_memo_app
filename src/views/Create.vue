024e2e32dc1baa5b3404059f71810632152c794b
gallardo <aiki.hosogaya@gmail.com>
2020-12-23 18:46:03 +0900

    change file name and create textarea and button

---
+++ src/views/Create.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <textarea v-model="newMemo"></textarea>
+    <button @click="createMemo">作成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["newMemo"],
+  methods: {
+    createMemo () {
+      this.$emit("create-click", this.newMemo)
+    }
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+  width: 300px;
+  height: 300px;
+}
+
+button {
+  margin: 3px;
+}
+</style>

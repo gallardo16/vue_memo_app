18f5ab22488ed1951e397ea79d01a848dddd91f1
gallardo <aiki.hosogaya@gmail.com>
2020-12-23 19:00:00 +0900

    import MemoIndex component and create methods and datas

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,59 @@
 <template>
   <div id="app">
-    <memo-header></memo-header>
-    <router-view></router-view>
-    <nav>
-      <router-link to="/">一覧</router-link>
-      <router-link to="/add">+</router-link>
-      <router-link to="/update">編集</router-link>
-    </nav>
+    <memo-header/>
+    <memo-index :memos="memos"></memo-index>
+    <router-view @create-click="createMemo" @update-click="updateMemo" @remove-click="removeMemo" :memos="memos"></router-view>
   </div>
 </template>
 
 <script>
 import MemoHeader from "./components/MemoHeader.vue"
+import MemoIndex from "./components/MemoIndex.vue"
 
 export default {
   components: {
-      MemoHeader
+    MemoHeader,
+    MemoIndex
   },
   data() {
     return {
+      memos: [],
+      newMemo: ''
+    }
+  },
+  mounted () {
+    if (localStorage.getItem('memos')) {
+      try {
+        this.memos = JSON.parse(localStorage.getItem('memos'))
+      } catch (e) {
+        localStorage.removeItem('memos')
+      }
+    }
+  },
+  methods: {
+    createMemo (value) {
+      this.newMemo = value
+      this.memos.push(this.newMemo)
+      this.newMemo = ''
+      this.saveMemos()
+      this.$router.push("/")
+    },
+    updateMemo (idx, value) {
+      this.memos[idx] = value
+      this.saveMemos()
+      this.$router.push("/")
+    },
+    removeMemo (idx) {
+      this.memos.splice(idx, 1)
+      this.saveMemos()
+      this.$router.push("/")
+    },
+    saveMemos () {
+      const parsed = JSON.stringify(this.memos)
+      localStorage.setItem('memos', parsed)
     }
   }
 }
 </script>
+
+

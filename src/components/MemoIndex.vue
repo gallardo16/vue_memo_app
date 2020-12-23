0b66c767e684bbc87661814c1cf5dcf4cc481689
gallardo <aiki.hosogaya@gmail.com>
2020-12-23 18:36:19 +0900

    create MemoIndex file

---
+++ src/components/MemoIndex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(memo, id) in memosFirstLine" :key="memo">
+        <a :href="/memos/ + id">{{ memo }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["memos"],
+  computed: {
+    memosFirstLine: function() {
+      return this.memos.map(value => value.split('\n')[0])
+    }
+  }
+}
+</script>

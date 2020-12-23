4da0dcc38cb34bf7a51e1f4515c60bc7da8cbae3
gallardo <aiki.hosogaya@gmail.com>
2020-12-23 18:49:50 +0900

    create update and delete button and make it editable

---
+++ src/views/Show.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>メモ詳細</h2>
+    <hr>
+    <textarea v-model="memos[id]"></textarea>
+    <button @click="updateMemo">編集</button>
+    <button @click="removeMemo">削除</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id", "memos"],
+  methods: {
+    updateMemo () {
+      this.$emit("update-click", this.id, this.memos[this.id])
+    },
+    removeMemo () {
+      this.$emit("remove-click", this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+  width: 300px;
+  height: 300px;
+}
+
+button {
+  margin: 3px;
+}
+</style>
